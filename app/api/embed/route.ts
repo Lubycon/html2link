f5b8bbabecc469a96d8703b0808f2129abdfbab2
evan-moon <bboydart91@gmail.com>
2025-06-14 16:04:52 +0900

    fix: Lubycon Org 내부 레포를 바라보게끔 변경

--- app/api/embed/route.ts
+++ app/api/embed/route.ts
@@ -5,24 +5,21 @@ const GITHUB_API = 'https://api.github.com';
 
 export async function POST(req: NextRequest) {
   const { html } = await req.json();
-  const id = uuidv4().slice(0, 8); // 짧은 ID로 파일명 구성
+  const id = uuidv4(); // 짧은 ID로 파일명 구성
   const filename = `widgets/${id}.html`;
 
-  const githubRes = await fetch(
-    `${GITHUB_API}/repos/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}/contents/${filename}`,
-    {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: `Add widget: ${id}`,
-        content: Buffer.from(html).toString('base64'),
-        branch: process.env.GITHUB_REPO_BRANCH,
-      }),
+  const githubRes = await fetch(`${GITHUB_API}/repos/lubycon/notion-embed/contents/${filename}`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({
+      message: `Add widget: ${id}`,
+      content: Buffer.from(html).toString('base64'),
+      branch: 'main',
+    }),
+  });
 
   const result = await githubRes.json();
 
