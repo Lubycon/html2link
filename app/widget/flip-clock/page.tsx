adfc7d36272c411748c3715a3012e8d418554873
evan-moon <bboydart91@gmail.com>
2025-07-06 06:02:38 +0900

    fix: remove padding

--- app/widget/flip-clock/page.tsx
+++ app/widget/flip-clock/page.tsx
@@ -4,7 +4,7 @@ import Clock from './Clock';
 
 export default function FlipClockPage() {
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', padding: '60px 0' }}>
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
       <Clock />
     </div>
   );
